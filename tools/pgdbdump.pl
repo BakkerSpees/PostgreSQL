ef374204085b8f6239c5581d65e057aa413ab788
sebastian@otherstuff.nl <sebastian@otherstuff.nl>
2019-06-17 15:44:49 +0200

    Added comment support in the regex input

--- tools/pgdbdump.pl
+++ tools/pgdbdump.pl
@@ -3,6 +3,7 @@ use strict;
 use DBIx::Simple;
 use File::Path;
 my $DEBUG = 0; 
+my $DRYRUN = 0;
 
 my $dump_dir = '/var/lib/postgresql/db_dump/10';
 my $bl_file = defined $ARGV[0]? $ARGV[0] : "/var/lib/postgresql/.db_dumb.blacklist";
@@ -11,14 +12,21 @@ my $pgdumpcom = "pg_dump -Fd -o -Z7 -w -U postgres";
 # Read file with blacklist matches
 open(my $fh, '<', $bl_file) or die "Cannot open $bl_file for reading";
 
-# read each line as an expression and add to one big expressiong
-my $rawregex = join( '|', map { chomp; $_ }  <$fh>  );
+# read each line as an expression and add to one big expression
+my @rrlist;
+while(my $line = <$fh>) {
+	chomp $line;
+	$line =~ s/\#.*?$//sm;   # remove comments
+	next if(!length($line)); # check for no value
+	push(@rrlist, $line);
+}
+my $rawregex = join('|', @rrlist);
 
 close($fh);
 
 # compile regex to save cpu time
-my $blacklist = qr/(?:$rawregex)/;
-print "Using regex: $blacklist\n" if($DEBUG);
+my $blacklist = qr/(?:$rawregex)/x;
+print "Using regex: $blacklist ($rawregex)\n" if($DEBUG);
 
 ## Connect and retrieve list of databases
 my $dbi = DBIx::Simple->connect('dbi:Pg:dbname=postgres', 'postgres') or die DBIx::Simple->error;
@@ -47,13 +55,12 @@ for my $dbname (@backupdbs) {
 	if (-e $dumplocation) {
 		if(-d $dumplocation) {
 			print "Removing old backup: $dumplocation\n" if($DEBUG);
-			rmtree($dumplocation);
+			rmtree($dumplocation) unless($DRYRUN);
 		} else {
 			die "$dumplocation exists, but not a directory, please double check and remove manually"
 		}
 	}
-
 	print "Dumping $dbname\n";
-	system("$pgdumpcom -d \"$dbname\" -f \"$dumplocation\"\n") and print "Error ($dbname): $!";
+	system("$pgdumpcom -d \"$dbname\" -f \"$dumplocation\"\n") and print "Error ($dbname): $!" unless($DRYRUN);
 }
 
